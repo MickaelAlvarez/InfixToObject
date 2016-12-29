68bd66387b817e3a16799c87cf2338200b0eb64d
Mickael Alvarez <alvarez.mickael.93@gmail.com>
2016-12-29 19:06:49 +0100

    Add Float numbers

---
+++ src/elements/number/NumberFloat.java
@@ -0,0 +1,30 @@
+package elements.number;
+
+public class NumberFloat implements INumber<Float> {
+	private Float value;
+	
+	public NumberFloat(Float value) {
+		this.value = value;
+	}
+
+	@Override
+	public Float getValue() {
+		return value;
+	}
+
+	@Override
+	public Float add(INumber<Float> other) {
+		return value + other.getValue();
+	}
+
+	@Override
+	public Float multiply(INumber<Float> other) {
+		return value * other.getValue();
+	}
+
+	@Override
+	public Float substract(INumber<Float> other) {
+		return value - other.getValue();
+	}
+
+}
