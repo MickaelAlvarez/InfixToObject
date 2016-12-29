40ca97d4cd0bb719bd42f4405add77b22145716b
Mickael Alvarez <alvarez.mickael.93@gmail.com>
2016-12-29 19:17:21 +0100

    Add Double factory

---
+++ src/elements/number/factory/NumberDoubleFactory.java
@@ -0,0 +1,12 @@
+package elements.number.factory;
+
+import elements.number.NumberDouble;
+
+public class NumberDoubleFactory implements NumberFactory<NumberDouble, Double>{
+
+	@Override
+	public NumberDouble create(Double param) {
+		return new NumberDouble(param);
+	}
+
+}
