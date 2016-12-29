2db8ee56496a0805fc61328e7230da7e9271a266
Mickael Alvarez <alvarez.mickael.93@gmail.com>
2016-12-29 11:42:45 +0100

    Remove useless Tests

--- test/equationElements/binaryOperator/integer/AddIntegerOperatorTest.java
+++ test/equationElements/binaryOperator/integer/AddIntegerOperatorTest.java
@@ -6,7 +6,6 @@ import org.junit.Test;
 
 import elements.binaryOperator.Adder;
 import elements.binaryOperator.IBinaryOperator;
-import elements.binaryOperator.Multiplier;
 import elements.number.NumberInteger;
 import elements.number.factory.NumberIntegerFactory;
 
@@ -45,30 +44,6 @@ public class AddIntegerOperatorTest {
 	IBinaryOperator<Integer> leftOperand = new Adder<Integer, NumberInteger>(new NumberInteger(value1), new NumberInteger(value2));
 	IBinaryOperator<Integer> rightOperand = new Adder<Integer, NumberInteger>(new NumberInteger(value3), new NumberInteger(value4));
 	IBinaryOperator<Integer> adder = new Adder<Integer, NumberInteger>(leftOperand, rightOperand, new NumberIntegerFactory());
-	assertEquals(expected, adder.solve().intValue());
-    }
-    
-    @Test
-    public void addMultiplierAndIntegerValuesTest() {
-	int value1 = 2, value2 = 20, value3 = 2, expected;
-	expected = value1 * value2 + value3;
-	IBinaryOperator<Integer> adder = new Adder<Integer, NumberInteger>(new Multiplier<Integer, NumberInteger>(new NumberInteger(value1), new NumberInteger(value2)), new NumberInteger(value3), new NumberIntegerFactory());
-	assertEquals(expected, adder.solve().intValue());
-    }
-    
-    @Test
-    public void addIntegerAndMultiplierValuesTest() {
-	int value1 = 2, value2 = 20, value3 = 2, expected;
-	expected = value1 + value2 * value3;
-	IBinaryOperator<Integer> adder = new Adder<Integer, NumberInteger>(new NumberInteger(value1), new Multiplier<Integer, NumberInteger>(new NumberInteger(value2), new NumberInteger(value3)), new NumberIntegerFactory());
-	assertEquals(expected, adder.solve().intValue());
-    }
-
-    @Test
-    public void addMultiplierAndMultiplierValuesTest() {
-	int value1 = 7, value2 = 3, value3 = 3, value4 = 7, expected;
-	expected = value1 * value2 + value3 * value4;
-	IBinaryOperator<Integer> adder = new Adder<Integer, NumberInteger>(new Multiplier<Integer, NumberInteger>(new NumberInteger(value1), new NumberInteger(value2)), new Multiplier<Integer, NumberInteger>(new NumberInteger(value3), new NumberInteger(value4)), new NumberIntegerFactory());
 	assertEquals(expected, adder.solve().intValue());
     }
     
--- test/equationElements/binaryOperator/integer/MultiplyIntegerOperatorTest.java
+++ test/equationElements/binaryOperator/integer/MultiplyIntegerOperatorTest.java
@@ -4,7 +4,6 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
-import elements.binaryOperator.Adder;
 import elements.binaryOperator.IBinaryOperator;
 import elements.binaryOperator.Multiplier;
 import elements.number.NumberInteger;
@@ -48,32 +47,4 @@ public class MultiplyIntegerOperatorTest {
 	assertEquals(expected, multiplier.solve().intValue());
     }
     
-    @Test
-    public void multiplyIntegerAndAdderValuesTest() {
-	int value1 = 21, value2 = 1, value3 = 1, expected;
-	expected = value1 * (value2 + value3);
-	IBinaryOperator<Integer> leftOperand = new Adder<Integer, NumberInteger>(new NumberInteger(value2), new NumberInteger(value3));
-	IBinaryOperator<Integer> multiplier = new Multiplier<Integer, NumberInteger>(new NumberInteger(value1), leftOperand, new NumberIntegerFactory());
-	assertEquals(expected, multiplier.solve().intValue());
-    }
-    
-    @Test
-    public void multiplyAdderAndIntegerValuesTest() {
-	int value1 = 1, value2 = 1, value3 = 21, expected;
-	expected = (value1 + value2) * value3;
-	IBinaryOperator<Integer> rigthOperand = new Adder<Integer, NumberInteger>(new NumberInteger(value1), new NumberInteger(value2));
-	IBinaryOperator<Integer> multiplier = new Multiplier<Integer, NumberInteger>(rigthOperand, new NumberInteger(value3), new NumberIntegerFactory());
-	assertEquals(expected, multiplier.solve().intValue());
-    }
-    
-    @Test
-    public void multiplyAdderAndAdderValuesTest() {
-	int value1 = 1, value2 = 1, value3 = 1, value4 = 20, expected;
-	expected = (value1 + value2) * (value3 + value4);
-	IBinaryOperator<Integer> rigthOperand = new Adder<Integer, NumberInteger>(new NumberInteger(value1), new NumberInteger(value2));
-	IBinaryOperator<Integer> leftOperand = new Adder<Integer, NumberInteger>(new NumberInteger(value3), new NumberInteger(value4));
-	IBinaryOperator<Integer> multiplier = new Multiplier<Integer, NumberInteger>(rigthOperand, leftOperand, new NumberIntegerFactory());
-	assertEquals(expected, multiplier.solve().intValue());
-    }
-
 }
